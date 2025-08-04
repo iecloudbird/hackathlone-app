79561723e4722b95c07b50c96ef1e2929af4c592
iecloudbird <kuroxxxshiro9@gmail.com>
2025-08-04 14:52:02 +0100

    fix: desugaring and omit packages for now

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // desugaring for specific packages
+        isCoreLibraryDesugaringEnabled = true 
     }
 
     kotlinOptions {
@@ -40,6 +42,11 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    // Core library desugaring for flutter_local_notifications
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
 }
 
 flutter {

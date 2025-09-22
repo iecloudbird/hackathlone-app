ef198a83680ef8f8621b764136fad9d14ffba9cd
iecloudbird <kuroxxxshiro9@gmail.com>
2025-09-22 15:30:09 +0100

    test: dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,30 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+// Basic unit tests for the Hackathon app
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hackathlone_app/main.dart';
+import 'package:hackathlone_app/core/constants/app_strings.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('App Constants Tests', () {
+    test('App strings are defined correctly', () {
+      expect(AppStrings.appTitle, isNotEmpty);
+      expect(AppStrings.appTitle, equals('Hackathlone App'));
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    test('Authentication strings work', () {
+      expect(AppStrings.loginTitle, equals('Welcome Back'));
+      expect(AppStrings.emailLabel, equals('Email'));
+      expect(AppStrings.passwordLabel, equals('Password'));
+    });
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  group('Basic Widget Tests', () {
+    test('Widget key generation works', () {
+      final key1 = Key('test_key_1');
+      final key2 = Key('test_key_2');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      expect(key1.toString(), contains('test_key_1'));
+      expect(key2.toString(), contains('test_key_2'));
+      expect(key1 != key2, isTrue);
+    });
   });
 }
